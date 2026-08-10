936dee35f33fec779c1203a3eba847a622d455ca
Mauro Sakugawa <maurotoshiuki@gmail.com>
2026-08-09 23:32:48 -0300

    feat(contacts): add contact input validation

---
+++ backend/src/Application/Contacts/InvalidContactInputException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AgendaInteligente\Application\Contacts;
+
+use InvalidArgumentException;
+
+final class InvalidContactInputException extends InvalidArgumentException
+{
+}
